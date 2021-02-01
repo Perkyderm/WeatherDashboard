be33d15314b6fc682d5e1d5c296af23d1f5d7c1c
Perkyderm <ncgoedecke@gmail.com>
2021-01-31 22:40:48 -0700

    added button to save to local storage and a function to recall the local storeage upon refresh.

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 var APIKey = "eeae76131291ea13fc8e4cd946e40faf";
 
 //todo link local storge to save searches
+//todo event listener for search city tie in above todo   use .trim()
+//todo add enter press
+//todo  icon rep
+// todo UV color corresponding identifiers
+//todo 5 day forecast that displays the date, icon rep if condisitons, temp, and humidity
+//todo search histroy need to go into  a button that can recall it - goes into already created event lsitener
+//*"history button"
 //todo
 
 $(document).ready(function () {
-  var locationSearch = function (location) {
+  //
+  //
+  //
+  var locationResult = function (location) {
     var queryUrl =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       location +
@@ -34,5 +44,16 @@ $(document).ready(function () {
       console.log(response);
     });
   };
-  locationSearch("Denver,Colorado");
+  //   locationResult("Seoul"); //*this is the default location
+
+  //! Button results
+  $("#searchbtn").on("click", function buttons() {
+    event.preventDefault();
+    var inputCity = $("#locationInput").val();
+    console.log(inputCity);
+    localStorage.setItem("City", inputCity);
+    locationResult(inputCity);
+  });
+
+  locationResult(localStorage.getItem("City"));
 });
