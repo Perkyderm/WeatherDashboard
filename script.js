fd499f6897ceb07fd03d140bd9434ecf43a2718d
Perkyderm <ncgoedecke@gmail.com>
2021-01-31 19:24:42 -0700

    set initail ajax for OpenWeather

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>;
+var APIKey = "eeae76131291ea13fc8e4cd946e40faf";
+
+//todo link local storge to save searches
+//todo
+
+$(document).ready(function () {
+  var locationSearch = function (location) {
+    var queryUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      location +
+      "&appid=" +
+      APIKey;
+
+    $.ajax({
+      url: queryUrl,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      //   var tempF = Math.floor(response.main.temp - 273.15) * 1.8 + 32;
+      //   $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+      //   $(".wind").text("Wind speed: " + response.wind.speed);
+      //   $(".humidity").text("Humidity: " + response.main.humidity);
+      //   $(".temp").text("Current temp in F: " + tempF);
+    });
+  };
+  locationSearch("Denver,Colorado");
+});
